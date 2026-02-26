05c187c13b6ef407785107bdede8e11559f1d5a3
sbsh.baek <sbsh.baek@samsung.com>
2026-02-26 14:13:30 +0900

    [WIP] Add Comments
    Signed-off-by: sbsh.baek <sbsh.baek@samsung.com>

--- dali-ui-foundation/devel-api/visual-factory/visual-factory.h
+++ dali-ui-foundation/devel-api/visual-factory/visual-factory.h
@@ -19,6 +19,7 @@
  */
 
 // EXTERNAL INCLUDES
+#include <dali/public-api/images/image-operations.h>
 #include <dali/public-api/object/base-handle.h>
 #include <dali/public-api/object/property-map.h>
 #include <dali/public-api/rendering/geometry.h>
@@ -35,23 +36,104 @@ namespace Internal DALI_INTERNAL
 class VisualFactory;
 }
 
+/**
+ * @brief VisualFactory is a singleton object that provides and shares visuals between controls
+ *
+ * By setting environment variable 'DALI_DEBUG_RENDERING', a debug visual is used which renders a quad wireframe.
+ *
+ * The visual type is required in the property map for requesting a visual.
+ *
+ * | %Property Name           | Type              |
+ * |--------------------------|-------------------|
+ * | visualType               | INTEGER or STRING |
+ * | shader                   | MAP               |
+ */
 class DALI_UI_API VisualFactory : public BaseHandle
 {
 public:
+  enum CreationOptions
+  {
+    NONE = 0,
+
+    IMAGE_VISUAL_LOAD_STATIC_IMAGES_ONLY = 1 << 0, ///< Load static images only when we use the image visual.
+  };
+
+  /**
+   * @brief Create or retrieve VisualFactory singleton.
+   *
+   * @return A handle to the VisualFactory control.
+   */
   static VisualFactory Get();
 
+  /**
+   * @brief Create a VisualFactory handle.
+   *
+   * Calling member functions with an uninitialised handle is not allowed.
+   */
   VisualFactory();
+
+  /**
+   * @brief Destructor
+   *
+   * This is non-virtual since derived Handle types must not contain data or virtual methods.
+   */
   ~VisualFactory();
+
+  /**
+   * @brief This copy constructor is required for (smart) pointer semantics.
+   *
+   * @param[in] handle A reference to the copied handle.
+   */
   VisualFactory(const VisualFactory& handle);
+
+  /**
+   * @brief This assignment operator is required for (smart) pointer semantics.
+   *
+   * @param [in] handle  A reference to the copied handle.
+   * @return A reference to this.
+   */
   VisualFactory& operator=(const VisualFactory& handle);
 
+  /**
+   * @brief Request the visual
+   *
+   * @param[in] propertyMap The map contains the properties required by the visual.
+   *            The content of the map determines the type of visual that will be returned.
+   * @return The handle to the created visual
+   */
   Visual::Base CreateVisual(const Property::Map& propertyMap);
 
+  /**
+   * @brief Get the default quad geometry which general visuals be used.
+   *
+   * @return The default quad geometry for visual factory.
+   */
   Dali::Geometry GetDefaultQuadGeometry();
 
+  /**
+   * @brief Enable or disable premultiplying alpha in images and image visuals.
+   *
+   * The default is to enable pre-multiplication on load.
+   *
+   * Applications that have assets with pre-multiplied alpha already applied should turn this option off.
+   *
+   * @param[in] preMultiply True if loaded images for image visuals should have alpha multiplied into the color
+   * channels.
+   */
   void SetPreMultiplyOnLoad(bool preMultiply);
+
+  /**
+   * @brief Get the setting for automatically pre-multiplying image visual images on load.
+   *
+   * @return True if loaded images have pre-multiplied alpha applied on load, false otherwise.
+   */
   bool GetPreMultiplyOnLoad() const;
 
+  /**
+   * @brief Discard visual base. It will keep reference of visual until idle callback called.
+   *
+   * @param[in] visual Discarded visual base.
+   */
   void DiscardVisual(Visual::Base visual);
 
 private:
--- dali-ui-foundation/internal/visuals/visual-base-impl.h
+++ dali-ui-foundation/internal/visuals/visual-base-impl.h
@@ -43,6 +43,8 @@ namespace Visual
 /**
  * Base class for all Visual implementations.
  * Stripped of transition/animation code for dali-ui.
+ *
+ * Note: The visual responds to the the Actor::COLOR by blending it with the 'Multiply' operator.
  */
 class Base : public BaseObject
 {
@@ -50,13 +52,14 @@ public:
   using FittingMode = DevelVisual::FittingMode;
 
   /**
-   * Setting the properties of the visual, this will
-   * be called by the factory with the properties in the map
+   * Setting the properties of the visual, this API should only called by the VisualFactory
+   * @param[in] propertyMap The properties for the requested Visual object.
    */
   void SetProperties(const Property::Map& propertyMap);
 
   /**
    * Set the visual on the actor (create renderers, etc)
+   * @pre Impl->mGeometry must be created before this method is called
    */
   void SetOnScene(Actor& actor);
 
@@ -72,136 +75,256 @@ public:
 
   /**
    * Set the mix color (tint)
+   * @param[in] mixColor The new mix color
    */
   void SetMixColor(const Vector4& color);
 
   /**
    * Get the mix color
+   * @return The current mix color
    */
   const Vector4& GetMixColor() const;
 
   /**
    * Perform an action
+   * @param[in] actionId The id of the action to perform
+   * @param[in] attributes The list of attributes for the action
    */
   void DoAction(const Property::Index actionId, const Property::Value& attributes);
+
+  /**
+   * Perform an action extension
+   * @param[in] actionId The id of the action to perform
+   * @param[in] attributes The list of attributes for the action
+   */
   void DoActionExtension(const Property::Index actionId, const Any& attributes);
 
   /**
-   * Set/Get name
+   * Set the visual name
+   * @param[in] name The name to give the visual
    */
   void SetName(const std::string& name);
+
+  /**
+   * Get the visual name
+   * @return The name of the visual
+   */
   const std::string& GetName() const;
 
   /**
-   * Set/Get depth index
+   * Set the depth index of this visual
+   * @param[in] index The depth index of this visual
    */
   void SetDepthIndex(int index);
+
+  /**
+   * Get the depth index of this visual
+   * @return The depth index of this visual
+   */
   int GetDepthIndex() const;
 
   /**
-   * Enable/disable premultiplied alpha
+   * Enable or disable premultiplied alpha
+   * @param[in] preMultiplied Whether alpha is pre-multiplied
    */
   void EnablePreMultipliedAlpha(bool preMultiplied);
+
+  /**
+   * Query whether alpha is pre-multiplied
+   * @return True if alpha is pre-multiplied, false otherwise
+   */
   bool IsPreMultipliedAlphaEnabled() const;
 
   /**
-   * Natural size
+   * Get the natural size of the visual
+   * @param[out] naturalSize The visual's natural size
    */
   virtual void GetNaturalSize(Vector2& naturalSize);
+
+  /**
+   * Get the height for a given width
+   * @param[in] width Width to use
+   * @return The height based on the width
+   */
   float GetHeightForWidth(float width);
+
+  /**
+   * Get the width for a given height
+   * @param[in] height Height to use
+   * @return The width based on the height
+   */
   float GetWidthForHeight(float height);
 
   /**
-   * Transform
+   * Set the transform and control size
+   * @param[in] transform A property map describing the transform
+   * @param[in] controlSize The size of the parent control for visuals that need to scale internally
    */
   void SetTransformAndSize(const Property::Map& transform, Size controlSize);
 
   /**
-   * Type
+   * Get the type of this visual
+   * @return The type of this visual
    */
   UI::Visual::Type GetType() const;
 
   /**
-   * Property queries
+   * Retrieve the property object associated with the property key
+   * @param[in] key The Property key of the visual
+   * @return The Property object
    */
   Dali::Property GetPropertyObject(Dali::Property::Key key);
 
   /**
-   * Is on scene
+   * Check if the visual is on scene
+   * @return True if this Visual is on the scene, false if it is off the scene
    */
   bool IsOnScene() const;
 
   /**
-   * Resource status
+   * Check if the visual resources are ready
+   * @return True if ready, false otherwise
    */
   bool IsResourceReady() const;
+
+  /**
+   * Get the resource status of the visual
+   * @return The loading status of visual resource
+   */
   UI::Visual::ResourceStatus GetResourceStatus() const;
 
   /**
-   * Observer management
+   * Set the event observer
+   * @param[in] observer The event observer to set
    */
   void SetEventObserver(Visual::EventObserver* observer);
+
+  /**
+   * Set the constraint observer
+   * @param[in] observer The constraint observer to set
+   */
   void SetConstraintObserver(Visual::ConstraintObserver* observer);
 
   /**
-   * Fitting mode
+   * Get the fitting mode for the visual
+   * @return The current fitting mode
    */
   FittingMode GetFittingMode() const;
 
   /**
    * Get the renderer
+   * @return The currently staged renderer, or an empty handle if not staged
    */
   Renderer GetRenderer() const;
 
   /**
-   * @brief Whether the visual has decoration (borderline, corner radius)
+   * Check if the visual has decoration (borderline, corner radius)
+   * @return True if the visual has decoration data
    */
   bool HasDecorationData() const;
 
 protected:
   /**
    * Constructor
+   * @param[in] factoryCache A pointer pointing to the VisualFactoryCache object
+   * @param[in] fittingMode The value that determines how the visual should be fit to the view
+   * @param[in] type The type of this visual
    */
   Base(VisualFactoryCache& factoryCache, FittingMode fittingMode, UI::Visual::Type type);
 
   /**
    * Destructor
+   * A reference counted object may only be deleted by calling Unreference().
    */
   ~Base() override;
 
   /**
-   * Virtual methods for derived classes
+   * Virtual methods for derived classes to set their properties
+   * Called by SetProperties() allowing sub classes to set their properties
+   * @param[in] propertyMap The properties for the requested Visual object
    */
   virtual void DoSetProperties(const Property::Map& propertyMap) = 0;
+
+  /**
+   * Called by SetOnScene() allowing sub classes to respond to the SetOnScene event
+   * @note The derived class is required to create the renderer, and add it to the actor when all the resources are in
+   * place
+   * @param[in] actor The actor applying this visual
+   */
   virtual void DoSetOnScene(Actor& actor) = 0;
+
+  /**
+   * Called by SetOffScene() allowing sub classes to respond to the SetOffScene event
+   * @param[in] actor The actor applying this visual
+   */
   virtual void DoSetOffScene(Actor& actor);
+
+  /**
+   * Called when transform or control size changes
+   * (Of use to SVG and Text visuals)
+   */
   virtual void OnSetTransform()
   {
   }
+
+  /**
+   * Called by CreatePropertyMap() allowing sub classes to respond to the CreatePropertyMap event
+   * @param[out] map The visual property map
+   */
   virtual void DoCreatePropertyMap(Property::Map& map) const = 0;
+
+  /**
+   * Called by CreateInstancePropertyMap() allowing derived classes to store instanced data
+   * (separate to styled data) that needs copying between visuals on state change
+   * @param[out] map The visual property map
+   */
   virtual void DoCreateInstancePropertyMap(Property::Map& map) const = 0;
+
+  /**
+   * Called by DoAction() allowing sub classes to do the given action
+   * @param[in] actionId The action to perform
+   * @param[in] attributes The list of attributes for the action
+   */
   virtual void OnDoAction(const Property::Index actionId, const Property::Value& attributes)
   {
   }
+
+  /**
+   * Called by DoActionExtension() allowing sub classes to do the given action
+   * @param[in] actionId The action to perform
+   * @param[in] attributes The list of attributes for the action
+   */
   virtual void OnDoActionExtension(const Property::Index actionId, const Any& attributes)
   {
   }
+
+  /**
+   * Generate a shader by the current properties from each sub classes's own shader-generate logic
+   * @return If the function defined, Generated shader by the current properties. Else, empty shader
+   */
   virtual Shader GenerateShader() const;
+
+  /**
+   * Update the shader when some properties are changed
+   */
   virtual void UpdateShader();
 
   /**
-   * @brief Notify resource ready to observer
+   * Notify resource ready to observer
+   * Called when the visuals resources are loaded / ready
+   * @param[in] resourceStatus The resource status
    */
   void ResourceReady(UI::Visual::ResourceStatus resourceStatus);
 
   /**
-   * @brief Initialise the renderer from the cache,
-   * if not available, create it and save it to the cache.
+   * Initialise the renderer from the cache
+   * If not available, create it and save it to the cache
    */
   void InitializeRenderer();
 
   /**
    * Register visual properties (mix color, etc)
+   * Register the uniform on the DecoratedVisualRenderer and store the property index if necessary
    */
   void RegisterDecoration();
 
